deb0a90d6929f8951268f18f87ea53d22f9a29e7
mpachling11-commits <mpachling11@gmail.com>
2026-01-12 13:02:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -67,7 +67,7 @@ def contact():
 
 @app.route("/booking")
 def booking():
-    return render_template("booking.html")
+    return render_template("booking.html", room="")
 
 @app.route("/book", methods=["POST"])
 def book_room():
@@ -126,3 +126,4 @@ def logout():
 # ---------------- RUN (RENDER SAFE) ----------------
 if __name__ == "__main__":
     app.run()
+
